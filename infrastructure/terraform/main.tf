0949ea7778ff04ea97e06b0e14d3273315f967ad
harshita-07Z <harshrajsinghchandrawat21@gmail.com>
2026-06-30 12:40:10 +0530

    AWS Security Group configuration

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -37,6 +37,17 @@ resource "aws_security_group" "web_sg" {
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-security-group"
+  }
 }
 
 resource "aws_instance" "web_server" {
